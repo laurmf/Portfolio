5ecea4d45ad672aab4ec8c074bd729438c9e46b7
laurmf <lmorenof28@gmail.com>
2022-03-11 14:49:53 +0100

    java script

--- js/funciones.js
+++ js/funciones.js
@@ -5,7 +5,34 @@ $(document).ready(function(){
         console.log("Clic en caja_linea") 
         $(".opciones").toggleClass("open")
     })
-
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        var movimiento= scroll/5;
+        console.log( "Scroll: "+scroll);
+        $("#cajita").css({
+            "transform":"translateY("+movimiento+"px)"
+        })
+        var movimiento= scroll/5;
+        console.log( "Scroll: "+scroll);
+        $("#cajita2").css({
+            "transform":"translateY("+movimiento+"px)"
+        })
+        var movimiento= scroll/8;
+        console.log( "Scroll: "+scroll);
+        $("#cajita3").css({
+            "transform":"translateY("+movimiento+"px)"
+        })
+        var movimiento= scroll/8;
+        console.log( "Scroll: "+scroll);
+        $("#cajita4").css({
+            "transform":"translateY(-"+movimiento+"px)"
+        })
+        var movimiento= scroll/5;
+        console.log( "Scroll: "+scroll);
+        $("#cajita5").css({
+            "transform":"translateY(-"+movimiento+"px)"
+        })
+    })
 
 
 
